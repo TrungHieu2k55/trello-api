13d3fa928eb2c03e0fd6b5798bec013c68648db0
TrungHieu2k5 <hieutnt.23itb@vku.udn.vn>
2026-02-18 17:05:31 +0700

    test router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,1 +1,15 @@
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRouter } from '~/routes/v1/boardRoute'
 
+const Router = express.Router()
+
+//Check APIs v1/status
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'APIs V1 are ready to use.' })
+})
+
+// Board API
+Router.use('/boards', boardRouter)
+
+export const APIs_V1 = Router
