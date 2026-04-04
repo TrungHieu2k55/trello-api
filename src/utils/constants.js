0fbe068f880d65a6c4b02231533e9e52c3beafa7
TrungHieu2k5 <hieutnt.23itb@vku.udn.vn>
2026-04-05 00:22:11 +0700

    Fix domain vercel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 // Những domain được phép truy cập tới server
 export const WHITELIST_DOMAINS =[
   // 'http://localhost:5173' éo cần
+  'https://trello-web-nine-beta.vercel.app/'
   //deploy lên domain
 ]
 
